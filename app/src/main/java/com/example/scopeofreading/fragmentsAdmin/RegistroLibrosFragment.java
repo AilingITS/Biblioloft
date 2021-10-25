459cd0b50230884ea44739389baa95548bfe734b
GonzaloGarza01 <gonzalo.garza01@hotmail.com>
2021-10-24 22:55:03 -0500

    Navegación de cada categoría en fragment_registro_libros Creación de cada fragment para cada categoría

--- app/src/main/java/com/example/scopeofreading/fragmentsAdmin/RegistroLibrosFragment.java
+++ app/src/main/java/com/example/scopeofreading/fragmentsAdmin/RegistroLibrosFragment.java
@@ -3,12 +3,33 @@ package com.example.scopeofreading.fragmentsAdmin;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.scopeofreading.R;
+import com.example.scopeofreading.fragmentsAdmin.agregarCategorias.agregarAventuraFragment;
+import com.example.scopeofreading.fragmentsAdmin.agregarCategorias.agregarCientificosFragment;
+import com.example.scopeofreading.fragmentsAdmin.agregarCategorias.agregarFiccionFragment;
+import com.example.scopeofreading.fragmentsAdmin.agregarCategorias.agregarFragmentPoeticos;
+import com.example.scopeofreading.fragmentsAdmin.agregarCategorias.agregarInfantilesFragment;
+import com.example.scopeofreading.fragmentsAdmin.agregarCategorias.agregarLiteraturaFragment;
+import com.example.scopeofreading.fragmentsAdmin.agregarCategorias.agregarMisteriosFragment;
+import com.example.scopeofreading.fragmentsAdmin.agregarCategorias.agregarRomanticosFragment;
+import com.example.scopeofreading.fragmentsAdmin.agregarCategorias.agregarTerrorFragment;
+import com.example.scopeofreading.fragmentsAdmin.registroLibros.registroAventuraFragment;
+import com.example.scopeofreading.fragmentsAdmin.registroLibros.registroCientificosFragment;
+import com.example.scopeofreading.fragmentsAdmin.registroLibros.registroFiccionFragment;
+import com.example.scopeofreading.fragmentsAdmin.registroLibros.registroInfantilesFragment;
+import com.example.scopeofreading.fragmentsAdmin.registroLibros.registroLiteraturaFragment;
+import com.example.scopeofreading.fragmentsAdmin.registroLibros.registroMisterioFragment;
+import com.example.scopeofreading.fragmentsAdmin.registroLibros.registroPoeticosFragment;
+import com.example.scopeofreading.fragmentsAdmin.registroLibros.registroRomanticosFragment;
+import com.example.scopeofreading.fragmentsAdmin.registroLibros.registroTerrorFragment;
 
 public class RegistroLibrosFragment extends Fragment {
 
@@ -17,6 +38,10 @@ public class RegistroLibrosFragment extends Fragment {
 
     private String mParam1;
     private String mParam2;
+
+
+    View vista;
+    private Button imgCientificos, imgLiteratura, imgAventura, imgTerror, imgMisterio, imgInfantiles, imgPoeticos, imgRomanticos, imgFiccion;
 
     public RegistroLibrosFragment() {
         // Required empty public constructor
@@ -44,6 +69,60 @@ public class RegistroLibrosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro_libros, container, false);
+        vista = inflater.inflate(R.layout.fragment_registro_libros, container, false);
+        imgCientificos = (Button) vista.findViewById(R.id.imgCientificos);
+        imgLiteratura = (Button) vista.findViewById(R.id.imgLiteratura);
+        imgAventura = (Button) vista.findViewById(R.id.imgAventura);
+        imgTerror = (Button) vista.findViewById(R.id.imgTerror);
+        imgMisterio = (Button) vista.findViewById(R.id.imgMisterio);
+        imgInfantiles = (Button) vista.findViewById(R.id.imgInfantiles);
+        imgPoeticos = (Button) vista.findViewById(R.id.imgPoeticos);
+        imgRomanticos = (Button) vista.findViewById(R.id.imgRomanticos);
+        imgFiccion = (Button) vista.findViewById(R.id.imgFiccion);
+
+        imgCientificos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { replaceFragment(new registroCientificosFragment()); }
+        });
+        imgLiteratura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { replaceFragment(new registroLiteraturaFragment()); }
+        });
+        imgAventura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { replaceFragment(new registroAventuraFragment()); }
+        });
+        imgTerror.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { replaceFragment(new registroTerrorFragment()); }
+        });
+        imgMisterio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { replaceFragment(new registroMisterioFragment()); }
+        });
+        imgInfantiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { replaceFragment(new registroInfantilesFragment()); }
+        });
+        imgPoeticos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { replaceFragment(new registroPoeticosFragment()); }
+        });
+        imgRomanticos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { replaceFragment(new registroRomanticosFragment()); }
+        });
+        imgFiccion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { replaceFragment(new registroFiccionFragment()); }
+        });
+
+        return vista;
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.body_container,fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/scopeofreading/fragmentsAdmin/registroLibros/registroFiccionFragment.java
@@ -0,0 +1,49 @@
+package com.example.scopeofreading.fragmentsAdmin.registroLibros;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.scopeofreading.R;
+
+public class registroFiccionFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public registroFiccionFragment() {
+        // Required empty public constructor
+    }
+
+    public static registroFiccionFragment newInstance(String param1, String param2) {
+        registroFiccionFragment fragment = new registroFiccionFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registro_ficcion, container, false);
+    }
+}
